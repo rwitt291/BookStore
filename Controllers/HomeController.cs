d7d898ad50a7f325ec1d6f1f7454dc04e387e97e
rwitt291 <90530778+rwitt291@users.noreply.github.com>
2022-02-16 22:50:26 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.Models;
+using BookStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRespository repo;
+
+        public HomeController (IBookstoreRespository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var b = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+            return View(b);
+        }
+    }
+}
---
+++ Models/EFBookstoreRespository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class EFBookstoreRespository : IBookstoreRespository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookstoreRespository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Books> Books => context.Books;
+    }
+}
